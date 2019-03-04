22809d28027151252a158e6f413412a2939033c4
murat16ertekin <murat_ertekin93@hotmail.com>
2019-03-04 10:58:34 +0300

    ikinci commit

--- App.js
+++ App.js
@@ -8,7 +8,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import codePush from 'react-native-code-push';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,12 +20,20 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  guncelle(){
+codePush.sync({
+  updateDialog: true,
+  installMode: codePush.InstallMode.IMMEDIATE
+})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native 1!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title='Güncelle' onPress={()=>{this.guncelle()}} />
       </View>
     );
   }
